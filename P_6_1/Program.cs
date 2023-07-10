d3be3ed4832992f8ae775f8c0f19ac31915e1692
S. Sergey <SS86RG@ya.ru>
2023-07-10 04:06:46 +0300

    seve

--- P_6_1/Program.cs
+++ P_6_1/Program.cs
@@ -1,67 +1,60 @@
-﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
-
-Console.Write("Введите числа через запятую: ");
-
-
-int[] StringToNum(string input)
+int imputed(string massege)
 {
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
+    Console.WriteLine(massege);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int[] Result(int lengt)
+{
+    int[] array = new int[lengt];
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
     {
-        if (input[i] == ',')
+        array[i] = imputed($"Введите {i+1}");
+        
+    }
+    return array;
+        
+}
+
+void PrintArrey(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        Console.WriteLine($"a[{i}]={arrey[i]}");
+    }
+    
+}
+
+int resultpositive (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
         {
             count++;
         }
+        
     }
-
-    int[] numbers = new int [count];
-    int index = 0;
-
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
-
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
+    return count;
 }
 
-int[] numbers = StringToNum(Console.ReadLine());
-int sum = 0;
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > 0)
-    {
-        sum++;
-    }
-}
-Console.WriteLine();
-Console.WriteLine($"количество значений больше 0 = {sum}");
+int lengt = imputed ("Введите значения > ");
+int[] array;
+array = Result(lengt);
+PrintArrey(array);
+Console.WriteLine($"количество значений больше 0 > {resultpositive(array)}");
 
-void PrintArray(int[] array)
-{
-    Console.Write("[ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.Write("]");
-}
-PrintArray(numbers);
+
+
+
+
+
+
